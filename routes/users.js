ce39a7846324ffec626c7f4a665e108f1c30eb64
Michael Wilson <piaonplayer868@hotmail.com>
2018-10-15 11:41:01 -0700

    Added the user schema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // Registers a user 
-router.get('/register', (req, res, next) => {
-    res.send("THERE IS A REGISTRATION GOING ON");
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name, 
+        email: req.body.email, 
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({
+                success: false, 
+                msg: "Couldn't add the user to the database."
+            });
+        } else {
+            res.json({
+                success: true, 
+                msg: "Successfully added the user to the database."
+            })
+        }
+    });
 });
 
 // Grabs data on the user, protected with token authentication 
